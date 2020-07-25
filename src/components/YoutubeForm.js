9f4f78cb0a209df1a1c0040282969b7d98f31036
Nityo <nityo.git@nityo.com>
2020-07-25 21:22:37 +0530

    touched with better validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -59,7 +59,7 @@ function YoutubeForm() {
                 <input type="text" id="name" name="name" 
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur} value={formik.values.name} />
-                {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+                { formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
                 </div>
 
                 <div className='form-control'>
@@ -68,7 +68,7 @@ function YoutubeForm() {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
                  value={formik.values.email} />
-                {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+                { formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
                 </div>
 
                 <div className='form-control'>
@@ -76,7 +76,7 @@ function YoutubeForm() {
                 <input type="text" id="channel" name="channel" 
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur} value={formik.values.channel} />
-                {formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+                { formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
                 </div>
 
                 <button type="submit">Submit</button>
