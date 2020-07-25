965a18d26043c66b2ff902686e65b09efe66c3e1
Nityo <nityo.git@nityo.com>
2020-07-25 22:42:01 +0530

    ErrorMessage Revisited

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import TextError from './TextError';
 
 const initialValues = {
     name: '',
@@ -33,13 +34,17 @@ function YoutubeForm() {
                 <div className='form-control'>
                 <label htmlFor="name">Name</label>
                 <Field type="text" id="name" name="name" />
-                <ErrorMessage className='error' name="name" component='div' />
+                <ErrorMessage name="name" component={TextError} />
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="email">Email</label>
                 <Field type="email" id="email" name="email" />
-                <ErrorMessage className="error" name="email" component='div'/>
+                <ErrorMessage name="email">
+                    {
+                        (errorMsg) => <div className='error'>{errorMsg}</div>
+                    }
+                </ErrorMessage>
                 </div>
 
                 <div className='form-control'>
