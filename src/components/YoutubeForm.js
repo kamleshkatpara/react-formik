7939643e92bb88f1e5081078414a9ab7b2801393
Nityo <nityo.git@nityo.com>
2020-07-25 22:02:41 +0530

    Form Component, Field Component & Error Component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormik } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const initialValues = {
@@ -12,32 +12,6 @@ const onSubmit = values => {
     console.log('Form Data', values);
 };
 
-// const validate = values => {
-//     // values.name, values.email, values.channel
-//     // errors.name, errors.email, errors.channel
-
-//     // errors.name = 'This field is required';
-
-//     let errors = {};
-
-//     if (!values.name) {
-//         errors.name = 'Please enter name';
-//     }
-
-//     if (!values.email) {
-//         errors.email = 'Please enter email address';
-//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//         errors.email = 'Invalid email format';
-//     }
-
-//     if (!values.channel) {
-//         errors.channel = 'Please enter channel name';
-//     }
-
-//     return errors;
-// }
-
-
 const validationSchema = Yup.object({
     name: Yup.string().required('Please enter name'),
     email: Yup.string().email('Invalid Email Format').required('Please enter email address'),
@@ -46,47 +20,35 @@ const validationSchema = Yup.object({
 
 function YoutubeForm() {
 
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema
-        //validate
-    });
-
-    // console.log('Form Values', formik.values);
-
-    // console.log('Form Errors', formik.errors);
-
-    console.log('Visited Fields', formik.touched);
-
     return (
-        <div>
-            <form onSubmit={formik.handleSubmit}>
+        <Formik 
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+        >
+            <Form>
                 
                 <div className='form-control'>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name"
-                { ...formik.getFieldProps('name') } />
-                { formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+                <input type="text" id="name" name="name" />
+                <ErrorMessage className='error' name="name" component='div' />
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" 
-                { ...formik.getFieldProps('email') } />
-                { formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+                <input type="email" id="email" name="email" />
+                <ErrorMessage className="error" name="email" component='div'/>
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" 
-                { ...formik.getFieldProps('channel') } />
-                { formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+                <input type="text" id="channel" name="channel" />
+                <ErrorMessage className="error" name="channel" component='div' />
                 </div>
 
                 <button type="submit">Submit</button>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
