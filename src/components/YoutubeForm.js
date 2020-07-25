928edde3370adf06d8c01ad28c2b666367dcf9c2
Nityo <nityo.git@nityo.com>
2020-07-25 21:18:34 +0530

    Fields Touched

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -46,7 +46,9 @@ function YoutubeForm() {
 
     // console.log('Form Values', formik.values);
 
-    console.log('Form Errors', formik.errors);
+    // console.log('Form Errors', formik.errors);
+
+    console.log('Visited Fields', formik.touched);
 
     return (
         <div>
@@ -54,19 +56,26 @@ function YoutubeForm() {
                 
                 <div className='form-control'>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+                <input type="text" id="name" name="name" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur} value={formik.values.name} />
                 {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+                <input type="email" id="email" name="email" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                 value={formik.values.email} />
                 {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
+                <input type="text" id="channel" name="channel" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur} value={formik.values.channel} />
                 {formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
                 </div>
 
