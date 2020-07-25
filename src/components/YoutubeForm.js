12dca7afb13469ade6729f3d5a7c0805fcc5dfc0
Nityo <nityo.git@nityo.com>
2020-07-25 17:27:59 +0530

    on submit

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -8,14 +8,17 @@ function YoutubeForm() {
             name: '',
             email: '',
             channel: ''
+        },
+        onSubmit: values => {
+            console.log('Form Data', values);
         }
     });
 
-    console.log('Form Values', formik.values);
+  // console.log('Form Values', formik.values);
 
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
 
@@ -25,7 +28,7 @@ function YoutubeForm() {
                 <label htmlFor="channel">Channel</label>
                 <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
 
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
