240b1504435c5259928f5410df4844267811296a
Nityo <nityo.git@nityo.com>
2020-07-25 21:36:06 +0530

    reducing code with formik functions

--- src/components/OldYoutubeForm.js
+++ src/components/OldYoutubeForm.js
@@ -65,22 +65,26 @@ function YoutubeForm() {
                 
                 <div className='form-control'>
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name"
-                { ...formik.getFieldProps('name') } />
+                <input type="text" id="name" name="name" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur} value={formik.values.name} />
                 { formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" name="email" 
-                { ...formik.getFieldProps('email') } />
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                 value={formik.values.email} />
                 { formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor="channel">Channel</label>
                 <input type="text" id="channel" name="channel" 
-                { ...formik.getFieldProps('channel') } />
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur} value={formik.values.channel} />
                 { formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
                 </div>
 
